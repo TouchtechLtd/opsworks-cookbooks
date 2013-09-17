bda80d803eb61963008686bfd4ffafcaaee40816
Alex Li <alex.zige@gmail.com>
2013-09-17 16:39:29 +1200

    update crontable command

--- scheduler/recipes/default.rb
+++ scheduler/recipes/default.rb
@@ -1,13 +1,17 @@
 node[:deploy].each do |application, deploy|
 
-  Chef::Log.info("Make sure you've got whenever gem installed and schedule.rb configed.")
+  Chef::Log.info("Make sure you've included 'whenever' gem and schedule.rb has configured.")
+
   Chef::Log.info("Setting up corn job for #{application}...")
 
-  execute "rake whenever to write to crontab" do
+  stack_name = node[:opsworks][:stack][:name]
+
+  execute "rake whenever to update crontab" do
     cwd "#{File.join(deploy[:deploy_to], 'current')}"
-    command "bundle exec whenever"
+    command "bundle exec whenever --update-crontab '#{stack_name}_#{deploy[:rails_env]}'"
+    action :run
   end
 
-  Chef::Log.info("Crontable has been updated")
+  Chef::Log.info("Crontable has been updated successfully!")
 
 end
