d678ca60cce68289ce1ab8197bd79cce9112196f
Alex Li <alex.zige@gmail.com>
2013-09-17 17:24:10 +1200

    add execute user for executing commands

--- scheduler/recipes/default.rb
+++ scheduler/recipes/default.rb
@@ -9,11 +9,11 @@ node[:deploy].each do |application, deploy|
   Chef::Log.debug("Executing command:  bundle exec whenever --update-crontab '#{stack_name}_#{deploy[:rails_env]}' in dir: #{File.join(deploy[:deploy_to], 'current')}")
 
   execute "rake whenever to update crontab" do
+    user node[:deploy][application][:user]
     cwd "#{File.join(deploy[:deploy_to], 'current')}"
     command "bundle exec whenever --update-crontab '#{stack_name}_#{deploy[:rails_env]}'"
     action :run
   end
 
   Chef::Log.info("Crontable has been updated successfully!")
-
 end
