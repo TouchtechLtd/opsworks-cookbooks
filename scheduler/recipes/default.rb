91236c6189199aca8a87480634947db8c774e928
Alex Li <alex.zige@gmail.com>
2013-09-17 16:56:30 +1200

    enable debugging

--- scheduler/recipes/default.rb
+++ scheduler/recipes/default.rb
@@ -1,10 +1,12 @@
 node[:deploy].each do |application, deploy|
 
-  Chef::Log.info("Make sure you've included 'whenever' gem and schedule.rb has configured.")
+  Chef::Log.info("Make sure you've included 'whenever' gem and schedule.rb has configged.")
 
   Chef::Log.info("Setting up corn job for #{application}...")
 
   stack_name = node[:opsworks][:stack][:name]
+
+  Chef::Log.Debug("Executing command:  bundle exec whenever --update-crontab '#{stack_name}_#{deploy[:rails_env]}' in dir: #{File.join(deploy[:deploy_to], 'current')}")
 
   execute "rake whenever to update crontab" do
     cwd "#{File.join(deploy[:deploy_to], 'current')}"
