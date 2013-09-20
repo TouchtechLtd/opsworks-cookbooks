a23018f37f2107a70b7382d326709afbb7142df1
Vimal Jobanputra <vim@bitbybit.io>
2013-09-20 13:52:13 +1200

    Create enclosing directories for environment variables

--- environment_variables/recipes/default.rb
+++ environment_variables/recipes/default.rb
@@ -4,21 +4,40 @@ node[:deploy].each do |application, deploy|
 
   if node[:env]
 
-    env_file_path = "#{deploy[:deploy_to]}/shared/config/environment_variables.rb"
-    Chef::Log.info("Env variables set, writing to #{env_file_path}")
-    template env_file_path do
-      cookbook "environment_variables"
-      source "environment_variables.rb.erb"
-      mode "0660"
+    Chef::Log.info("Env variables set")
+
+    env_dir_path = "#{deploy[:deploy_to]}/shared/config"
+    Chef::Log.info("Creating enclosing directory #{env_dir_path} if it doesn't exist")
+    directory env_dir_path do
       owner node[:deploy][application][:user]
       group node[:deploy][application][:group]
+      mode 0770
+      recursive true
+      action :create
+      only_if do
+        !File.exists?(env_dir_path)
+      end
+    end
+
+    env_file_path = "#{deploy[:deploy_to]}/shared/config/environment_variables.rb"
+    Chef::Log.info("Writing variables to #{env_file_path}")
+    template env_file_path do
+      cookbook 'environment_variables'
+      source 'environment_variables.rb.erb'
+      owner node[:deploy][application][:user]
+      group node[:deploy][application][:group]
+      mode 0660
       variables(
         :env => node[:env]
       )
     end.run_action(:create)
+
     Chef::Log.info("Env variables written")
+
   else
+
     Chef::Log.info("Env variables not set")
+
   end
 
 end
