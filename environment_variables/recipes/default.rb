28c42caf3fb1c533a95db3dc335c217c315cf8ed
Paul Tuckey <paul@tuckey.org>
2014-03-20 16:47:56 +1300

    final tidy up

--- environment_variables/recipes/default.rb
+++ environment_variables/recipes/default.rb
@@ -4,7 +4,7 @@ node[:deploy].each do |application, deploy|
 
   if node[:env]
 
-    Chef::Log.info("Env variables set test1")
+    Chef::Log.info("Env variables set")
 
     directory "#{deploy[:deploy_to]}/shared/config" do
       owner "deploy"
@@ -25,30 +25,6 @@ node[:deploy].each do |application, deploy|
     file File.join(deploy[:deploy_to], 'shared', 'config', 'environment_variables.rb') do
       content env_vars_content
     end
-
-    #env_dir_path = "#{deploy[:deploy_to]}/shared/config"
-    #Chef::Log.info("Creating enclosing directory #{env_dir_path} if it doesn't exist")
-    #directory env_dir_path do
-    #  owner node[:deploy][application][:user]
-    #  group node[:deploy][application][:group]
-    #  mode 0770
-    #  recursive true
-    #  action :create
-    #end
-
-    #  /srv/www/posboss/shared/config/environment_variables.rb
-    #env_file_path = "#{deploy[:deploy_to]}/shared/config/environment_variables.rb"
-    #Chef::Log.info("Writing variables to #{env_file_path}")
-    #template env_file_path do
-    #  cookbook 'environment_variables'
-    #  source 'environment_variables.rb.erb'
-    #  owner node[:deploy][application][:user]
-    #  group node[:deploy][application][:group]
-    #  mode 0660
-    #  variables(
-    #    :env => node[:env]
-    #  )
-    #end.run_action(:create)
 
     Chef::Log.info("Env variables written")
 
