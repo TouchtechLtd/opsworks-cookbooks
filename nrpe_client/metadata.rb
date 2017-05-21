3c85be09de27f0090225c3e60b57ec8f49c7f95d
Sam F <sam@touchtech.co.nz>
2017-05-22 10:09:30 +1200

    Adding nrpe_client to berksfile, removing version check

--- nrpe_client/metadata.rb
+++ nrpe_client/metadata.rb
@@ -1,12 +1,7 @@
 name 'nrpe_client'
-maintainer 'The Authors'
-maintainer_email 'you@example.com'
-license 'All Rights Reserved'
 description 'Installs/Configures nrpe_client'
 long_description 'Installs/Configures nrpe_client'
 version '0.1.0'
-chef_version '>= 12.1' if respond_to?(:chef_version)
-
 # The `issues_url` points to the location where issues for this cookbook are
 # tracked.  A `View Issues` link will be displayed on this cookbook's page when
 # uploaded to a Supermarket.
